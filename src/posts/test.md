482459ed068f0bfc0eeeb91a8064b096d095b4e9
erulabs <seandon.mooy@gmail.com>
2016-03-10 01:11:16 -0800

    fix markdown req

--- src/posts/test.md
+++ src/posts/test.md
@@ -1,2 +1,3 @@
 # Hello World
 Post body
+sdfsdf
--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
     loaders: [
 			{ test: /\.less$/, loader: 'style!css!less' },
       { test: /\.css$/, loader: 'style!css' },
-      { test: /\.md$/, loader: 'html!markdown?gfm=false' },
+      { test: /\.md$/, loader: 'raw' },
 			{ test: /\.(jpe?g|png|gif|svg)$/i, loaders: [ 'url?limit=8192', 'img' ] },
 			{ test: /\.jsx$/, exclude: /node_modules/,
 				loaders: [ 'react-hot', 'babel?presets[]=stage-0,presets[]=react,presets[]=es2015' ] }
