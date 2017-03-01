33345cccbaefb1a6f65b241fc3d3353b222ca3cc
erulabs <seandon.mooy@gmail.com>
2017-02-28 20:31:12 -0800

    yarn lock

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -3,11 +3,12 @@ source ./bin/_variables.sh
 
 echo "Uploading to ${DEST} ..."
 
-rsync -arc --delete --delete-after ./inf ./_build ${DEST}/
-
-ssh ${SERVER} "\
-  sudo /bin/cp /home/circleci/seandonmooy.com/inf/nginx/seandonmooy.conf /etc/nginx/sites-enabled/seandonmooy.conf && \
-  sudo /usr/sbin/service nginx reload
-"
+rsync -arc --delete --delete-after ./inf ./_build ${DEST}/ | fgrep "nginx" > /dev/null && {
+  echo "Reloading nginx"
+  ssh ${SERVER} "\
+    sudo /bin/cp /home/circleci/seandonmooy.com/inf/nginx/seandonmooy.conf /etc/nginx/sites-enabled/seandonmooy.conf && \
+    sudo /usr/sbin/service nginx reload
+  "
+}
 
 echo "Done!"
