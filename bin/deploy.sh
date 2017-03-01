cfbea5f48a276232944b6ce999a33d26fe340499
erulabs <seandon.mooy@gmail.com>
2017-02-28 20:24:19 -0800

    super fast deploy

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -3,10 +3,10 @@ source ./bin/_variables.sh
 
 echo "Uploading to ${DEST} ..."
 
-rsync -arv --delete --delete-after ./inf ./_build ${DEST}/
+rsync -arc --delete --delete-after ./inf ./_build ${DEST}/
 
 ssh ${SERVER} "\
-  sudo /bin/cp -v /home/circleci/seandonmooy.com/inf/nginx/seandonmooy.conf /etc/nginx/sites-enabled/seandonmooy.conf && \
+  sudo /bin/cp /home/circleci/seandonmooy.com/inf/nginx/seandonmooy.conf /etc/nginx/sites-enabled/seandonmooy.conf && \
   sudo /usr/sbin/service nginx reload
 "
 
