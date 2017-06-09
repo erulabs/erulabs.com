4628fbc52f97df32473c419a23284478ca77b2b2
erulabs <seandon.mooy@gmail.com>
2017-06-08 20:16:25 -0700

    buildPages

--- buildPages.js
+++ buildPages.js
@@ -39,6 +39,8 @@ module.exports = function () {
           (i + 1) +
           '. <a href="/posts/' +
           results[i].replace('.md', '.html') +
+          '?v=' +
+          time +
           '" alt="' +
           title +
           '">',
