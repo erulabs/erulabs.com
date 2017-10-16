ce5cbf9c63e8314eb6e7acb6033052cda3d3c947
erulabs <seandon.mooy@gmail.com>
2017-10-16 14:03:47 -0700

    simpler deploy

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -8,6 +8,8 @@ echo "Uploading to ${DEST} ..."
 rsync -arc --delete --delete-after ./inf ./_build ${DEST}/ | fgrep "nginx" > /dev/null && {
   echo "Reloading nginx"
   ssh ${SERVER} "\
+    ! command -v nginx && apt-get install -yqq nginx && \
+    mkdir -p /www/ && \
     sudo /bin/cp /home/circleci/erulabs.com/inf/nginx/seandonmooy.conf /etc/nginx/sites-enabled/seandonmooy.conf && \
     sudo /usr/sbin/service nginx reload
   "
