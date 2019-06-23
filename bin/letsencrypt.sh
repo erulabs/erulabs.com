e083d8e9f018a0f99e8e521393c780c9079f98a2
Seandon Mooy <seandon.mooy@gmail.com>
2019-06-23 00:41:08 -0700

    fixes for LE and nginx

--- bin/letsencrypt.sh
+++ bin/letsencrypt.sh
@@ -12,6 +12,10 @@ certbot -n \
   --expand \
   -d erulabs.com \
   -d seandonmooy.com \
+  -d mooy.app \
+  -d www.erulabs.com \
+  -d www.seandonmooy.com \
+  -d www.mooy.app \
   -m seandon.mooy@gmail.com \
   --agree-tos --manual-public-ip-logging-ok \
   --manual-auth-hook "./bin/letsencrypt_hooks.sh hook" \
@@ -23,8 +27,8 @@ cp tmp/le/live/erulabs.com/*.pem secrets/
 rsync -arvc ./secrets ${DEST}/
 
 ssh ${SERVER} "\
-	chown -R eru:www-data ${DEST} && \
-        chmod 640 ${DEST}/secrets/* && \
+	chown -R eru:www-data  && \
+        chmod 640 ${DEST_DIR}/secrets/* && \
 	sudo /usr/sbin/service nginx reload"
 
 rm -rf src/.well-known
