78d38cc97c943119704d9f42a2642a9a13c4a9df
Seandon Mooy <seandon.mooy@gmail.com>
2019-09-03 17:33:11 -0700

    CRA

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Hello, its erulabs.com
+      </header>
+    </div>
+  );
+}
+
+export default App;
